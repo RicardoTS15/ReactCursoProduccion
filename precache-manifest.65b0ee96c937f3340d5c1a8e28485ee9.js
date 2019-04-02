98253953ca13f7d3c0aacdd4dd7686c329238536
RicardoTS15 <ricardots211@gmail.com>
2019-04-01 20:09:25 -0600

    Primer commit

---
+++ precache-manifest.65b0ee96c937f3340d5c1a8e28485ee9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2bec6ede9d13752083bd",
+    "url": "/static/js/main.b6bb93f3.chunk.js"
+  },
+  {
+    "revision": "cc7a04f15380de3b946d",
+    "url": "/static/js/2.e7b9c5f8.chunk.js"
+  },
+  {
+    "revision": "cc7a04f15380de3b946d",
+    "url": "/static/css/2.f1b23f55.chunk.css"
+  },
+  {
+    "revision": "1177a49a8630eaf84468fcc53a197b87",
+    "url": "/index.html"
+  }
+];
